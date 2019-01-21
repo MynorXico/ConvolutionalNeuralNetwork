1b97866704da79371f006d355e0c4cce8d48cb4a
Mynor Xico <xic14659@uvg.edu.gt>
2019-01-21 01:11:54 -0600

    Forward propagation for numpy convnn model

---
+++ numpy_model.py
@@ -0,0 +1,153 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)   # So tests remain consistent
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image,
+    as illustrated in Figure 1.
+
+    Argument:
+    X -- python numpy array of shape (m, n_h, n_W, n_c) representing a batch of m images.
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+
+    Returns:
+    X_pad -- padded image of shape (m, n_h + 2*pad, n_W + 2*pad, n_C)
+    """
+
+    X_pad = np.pad(X, ((0,0), (pad, pad), (pad, pad), (0,0)), 'constant', constant_values = (0, 0))
+    return X_pad
+
+
+""" Verify it's working fine :D
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 2)
+print("x.shape = ", x.shape)
+print("x_pad.shape =", x_pad.shape)
+print("x[1,1] =", x[1,1])
+print("x_pad[1,1]=", x_pad[1,1])
+
+fig, axarr = plt.subplots(1,2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0,:,:,0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0,:,:,0])
+plt.show()
+"""
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation
+    of the previous layer
+
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_c_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+
+    Returns:
+    Z -- a scalar value, result of convolving the sliding window (W, b) on a slice x of the input data
+    """ 
+    # Element-wise product between a_slice and W. Without adding bias yet.
+    s = a_slice_prev*W
+
+    # Sum over all entries of the volume s
+    Z = np.sum(s)
+
+    # Add bias b to Z, casting b to float() so that Z results in a scalar value
+    Z = Z + float(b)
+
+    return Z
+
+""" Verify it's working fine :Dnp.random.seed(1)
+a_slice_prev = np.random.randn(4,4,3)
+W = np.random.randn(4,4,3)
+b = np.random.randn(1,1,1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z = ", Z)
+assert Z == -6.999089450680221
+"""
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a vonvolution funcion
+
+    Arguments:
+    A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_Prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1,1,1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_h, n_W, n_C)
+    cache -- cache of values needed fo the conv_backward() function
+    """
+
+    # Retrieve dimensions from A_prev's shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrive dimensions from W's shape
+    (f, f, n_C_prev, n_C) = W.shape
+
+    # Retrieve information from "hparameters"
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+
+    # Dimensions of the CONV output volume.
+    n_H = int((1/stride)*(n_H_prev + 2*pad - f))+1
+    n_W = int((1/stride)*(n_W_prev + 2*pad - f))+1
+
+    # Initialize the output volume Z with zeros
+    Z = np.zeros((m, n_H, n_W, n_C))
+    
+    # Create A_prev_pad by padding A_prev
+    A_prev_pad = zero_pad(A_prev, pad)
+    print(A_prev_pad)
+    for i in range(m):                                          # Loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]                              # Select ith training example's padding activation
+        for h in range(n_H):                                    # Loop over the vertical axis of the output volume
+            for w in range(n_W):                                # Loop over the horizontal axis of the output volume
+                for c in range(n_C):                            # Loop over the channels (filters) of the output volume
+                    # Find the current "slice"
+                    vert_start = h*f
+                    vert_end = (h+1)*f
+                    horiz_start = w*f
+                    horiz_end = (w+1)*f
+
+                    # Use the corners to define the slice of a_prev_pad
+                    a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end]
+
+                    # Convolve the (3D) slice with the correct filter w and bias b, to get back one output neuron
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, W[:,:,:,c], b[:,:,:,c])
+
+    # Making sure output shape is correct
+    assert(Z.shape == (m, n_H, n_W, n_C))
+
+    # cache information for backpropagation
+    cache = (A_prev, W, b, hparameters)
+
+    return Z, cache
+
+"""
+Testing everything is okay :S """
+np.random.seed(1)
+A_prev = np.random.randn(10, 4, 4, 3)
+W = np.random.randn(2, 2, 3, 8)
+b = np.random.randn(1, 1, 1, 8)
+hparameters = {
+        "pad":2,
+        "stride":2
+    }
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+print("Z's mean = ", np.mean(Z))
+print("Z[3,2,1] = ", Z[3,2,1])
+print("cache_conv[0][1][2][3] = ", cache_conv[0][1][2][3])
